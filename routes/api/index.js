668a0dcb1e4027e3bb06cadfadb6e7b83484ba65
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-07 23:38:58 +0300

    add re-export router

---
+++ routes/api/index.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+
+const usersRouter = require('./users');
+const reviewsRouter = require('./reviews');
+const tasksRouter = require('./tasks');
+
+const router = Router();
+
+router.use('/users', usersRouter);
+router.use('./reviews', reviewsRouter);
+router.use('./tasks', tasksRouter);
+
+module.exports = router;
