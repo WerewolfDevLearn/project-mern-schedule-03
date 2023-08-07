ba7caf1d0bd655aff2e26cbbaa03ef25777364b6
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-08 00:00:36 +0300

    created the middleware function that checks the validity of an object ID parameter (id) in a request

---
+++ middlewares/isValidId.js
@@ -0,0 +1,14 @@
+const { isValidObjectId } = require('mongoose');
+const { HttpError } = require('../utils/HttpError');
+
+const isValidId = (req, res, next) => {
+  const { id } = req.params;
+
+  if (!isValidObjectId(id)) {
+    next(HttpError(400, `${id} is not a valid id`));
+  }
+
+  next();
+};
+
+module.exports = isValidId;
