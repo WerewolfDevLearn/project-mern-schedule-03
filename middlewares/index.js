ea898a400c131079c2f3b9b7519358dc83c54d7a
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-08 00:08:15 +0300

    added re-export

---
+++ middlewares/index.js
@@ -0,0 +1,9 @@
+const isValidId = require('./isValidId');
+const globalErrorHandler = require('./globalErrorHandler');
+const missingRouteHandler = require('./missingRouteHandler');
+
+module.exports = {
+  isValidId,
+  globalErrorHandler,
+  missingRouteHandler,
+};
