e5b56ec5a75827cb3865e48ea069f7ee1f513e66
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-09 22:07:07 +0300

    add re-export controllers

---
+++ controllers/tasks/index.js
@@ -0,0 +1,11 @@
+const getAllTasks = require('./getAllTasks');
+const addTask = require('./addTask');
+const updateTask = require('./update');
+const deleteTask = require('./deleteTask');
+
+module.exports = {
+  getAllTasks,
+  addTask,
+  updateTask,
+  deleteTask,
+};
