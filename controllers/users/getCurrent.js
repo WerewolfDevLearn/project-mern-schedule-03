a5022fa623d23cf84500f2a854e64c58025a459f
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2023-08-10 12:34:54 +0300

    fix respons fet current

--- controllers/users/getCurrent.js
+++ controllers/users/getCurrent.js
@@ -1,16 +1,17 @@
 const { ctrlWrapper } = require('../../decorators');
 
 const getCurrent = ctrlWrapper(async (req, res) => {
-  const { name, email, phone, birthday, skype, avatarUrl } = req.user;
+  const { name, email, phone, birthday, skype, avatarUrl, _id, verifiedEmail } = req.user;
   res.status(200).json({
     user: {
-      name: returntUser.name,
-      email: returntUser.email,
-      phone: returntUser.phone,
-      birthday: returntUser.birthday,
-      avatarUrl: returntUser.avatarUrl,
-      _id: returntUser._id,
-      verifiedEmail: returntUser.verifiedEmail,
+      name,
+      email,
+      phone,
+      birthday,
+      avatarUrl,
+      _id,
+      verifiedEmail,
+      skype,
     },
   });
 });
