a4e8516f8f7a56a066cfd77787af7dc2e80868ae
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2023-08-10 12:30:21 +0300

    add reponse for getCurrent

--- controllers/users/getCurrent.js
+++ controllers/users/getCurrent.js
@@ -2,7 +2,17 @@ const { ctrlWrapper } = require('../../decorators');
 
 const getCurrent = ctrlWrapper(async (req, res) => {
   const { name, email, phone, birthday, skype, avatarUrl } = req.user;
-  res.status(200).json({ name, email, phone, birthday, skype, avatarUrl });
+  res.status(200).json({
+    user: {
+      name: returntUser.name,
+      email: returntUser.email,
+      phone: returntUser.phone,
+      birthday: returntUser.birthday,
+      avatarUrl: returntUser.avatarUrl,
+      _id: returntUser._id,
+      verifiedEmail: returntUser.verifiedEmail,
+    },
+  });
 });
 
 module.exports = getCurrent;
