ec64829e4bb0e06bf89fc45176f08320e3399306
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2023-08-13 18:59:23 +0300

    add token to current

--- controllers/users/getCurrent.js
+++ controllers/users/getCurrent.js
@@ -3,6 +3,7 @@ const { ctrlWrapper } = require('../../decorators');
 const getCurrent = ctrlWrapper(async (req, res) => {
   const { name, email, phone, birthday, skype, avatarUrl, _id, verifiedEmail } = req.user;
   res.status(200).json({
+    token: req.user.token,
     user: {
       name,
       email,
