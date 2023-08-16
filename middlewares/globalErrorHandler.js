dc3e9b24f3934b709fa0786717e102ca3a834fd0
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-16 18:08:11 +0300

    complete message

--- middlewares/globalErrorHandler.js
+++ middlewares/globalErrorHandler.js
@@ -1,5 +1,8 @@
 const globalErrorHandler = (err, req, res, next) => {
-  const { status = 500, message = 'Somethitd went wrong!' } = err;
+  const {
+    status = 500,
+    message = 'Oops, it seems we`ve encountered an unexpected hiccup. Our team is already on the case to address this issue. Please bear with us while we work to restore things to normal',
+  } = err;
 
   res.status(status).json({ message });
 };
